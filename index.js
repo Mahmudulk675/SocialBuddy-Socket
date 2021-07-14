6a54f267feec78bd1d54b99ffe80738ef940d7fa
Mahmudulk675 <mahmudulk675@gmail.com>
2021-07-14 16:14:52 +0600

    make it better

---
+++ index.js
@@ -0,0 +1,45 @@
+const cors = require("cors");
+const io = require("socket.io")(8080, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.id === userId) && users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.filter((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+  // After every connection take the user Id and socket Id in the users array
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUser", users);
+  });
+
+  // send message and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  // disconnect
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+    removeUser(socket.id);
+    io.emit("getUser", users);
+  });
+});
